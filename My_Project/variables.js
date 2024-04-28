721ca615e7d782d1b0fa07c4ff1d9e32c0057661
RUMAISAKAMAL <140235855+RUMAISAKAMAL@users.noreply.github.com>
2024-04-28 20:39:06 +0000

    ts

---
+++ My_Project/variables.js
@@ -0,0 +1,16 @@
+const accountid=12345
+let accountemail= "abc@gmail.com"
+var accountpass= "12345"
+accountcity= "karachi"
+let accountstate;
+
+//accountid= 2//not allowed
+
+console.log(accountid);
+
+accountpass="125"
+accountcity="lahore"
+accountemail= "xyz@gmail.com"
+
+console.log(accountid);
+console.table([accountid, accountpass, accountemail, accountcity, accountstate]);
