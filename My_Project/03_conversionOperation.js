4d8ae7b31efafc41dcdaccc8d0707caf06658cf0
RUMAISAKAMAL <140235855+RUMAISAKAMAL@users.noreply.github.com>
2024-04-30 16:19:02 +0000

    config

--- My_Project/03_conversionOperation.js
+++ My_Project/03_conversionOperation.js
@@ -1,4 +1,20 @@
-let somenumber=33
-let stringnumber=String (somenumber)
-console.log(stringnumber);
-console.log(typeof (stringnumber));
+//let somenumber=33
+//let stringnumber=String (somenumber)
+//console.log(stringnumber);
+//console.log(typeof (stringnumber));//
+//************operations***********//
+let value =3
+let negvalue=-value
+//console.log(negvalue)
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2%2);
+//console.log(2/2);
+let str1="Rumaisa"
+let str2=" Babar Azam"
+let str= str1+str2
+//console.log(str)
+let gamecounter=100;
+ ++gamecounter
+ console.log(gamecounter);
